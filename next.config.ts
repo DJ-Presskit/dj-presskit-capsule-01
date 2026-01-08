732b19f23add2d2ecbf34cb2fe2049731d96136b
rfd <ramirofazioyt@gmail.com>
2026-01-08 01:36:36 -0300

    feat: default SEO

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // ✅ FIX: Forzar que todos los assets (/_next/*) se sirvan desde la cápsula
-
-  // Esto evita “white screen” cuando el HTML se entrega vía router con dominio limpio.
-
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
